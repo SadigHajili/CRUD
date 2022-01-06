370009912c4d50e4cd87c8c2ab18d90d6f4ee6f1
Hajili <tu201806363@aztu.local>
2022-01-06 13:59:22 +0400

    first commit

---
+++ CRUD01/Models/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CRUD01.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUD01.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Position> Positions { get; set; }
+
+    }
+}
---
+++ CRUD01/Models/Position.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUD01.Models
+{
+    public class Position
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Employee> Employees = new List<Employee>();
+    }
+}
